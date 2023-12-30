2367ea0018b3fec2edb40b0fa336108c4daaaf5a
Hermes Lima <hermes.oficial@gmail.com>
2023-12-30 19:34:24 -0300

    Adicionando alteração em 30-12-2023

--- src/server.js
+++ src/server.js
@@ -1,34 +1,54 @@
-const express = require('express')
-const path = require('path')
+// Módulos instalados
+const express = require("express");
+const path = require("path");
 
+const client = require("./database");
+
+// Módulos criados pelo usuário
+// Não precisa colocar o nome index.js, pois ele tem o nome de index e é único na pasta
 //const db = require('./database')
-//const routes = require('./routes')
-const mongoose = require('mongoose')
-
+const routes = require("./routes");
 
 // inicializa o express
-const app = express()
+const app = express();
 
+///////////////////////////////////////////////////////////
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://hermesoficial:fefenina@bdtest.ksgevmw.mongodb.net/";
+const databasename = "escola";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
+// conexão com o BD
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    await client.connect().then((client) => {
+      
+      const connect = client.db(databasename);
+    
+      // New Collection
+      const collection = connect
+          .createCollection("atletas");
+    
+          console.log("collection created", collection);
+          console.log(collection);
+        }).catch((err) => {
+    
+      // Handling the error 
+      console.log(err.Message);
+  })
+;
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+
+
+    // New Collection
+
+
+    
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -36,46 +56,31 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-
-
-
+////////////////////////////////////////////////////////////
 
 // definindo o template engine. Por padrão, a pasta views sempre será procurada na raiz do projeto
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 // a pasta padrão será procurada. Como o server.js está dentro da pasta src ele vai buscar a partir daqui
 // __dirname = C:\Users\Maria\workspace\nodejs\projeto-crud\src
-app.set('views', path.join(__dirname, 'views'))
+app.set("views", path.join(__dirname, "views"));
 
 // definindo os arquivos públicos
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-console.log(__dirname)
+console.log(__dirname);
 
 // habilita server para receber dados via post (formulário)
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // definindo as rotas
-//app.use('/', routes)
-
-// rotas  
-app.get('/', (req, res) => {
-    res.render('index', { 
-        title: "Título teste"
-    })
-})
+app.use("/", routes);
 
 // 404 error (not found)
-app.use((req, res) => { // middleware
-  res.send('Página não encontrada!')
-})
+app.use((req, res) => {
+  // middleware
+  res.send("Página não encontrada!");
+});
 
 // executando o servidor
-const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`Server is listening on port ${port}`))
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
